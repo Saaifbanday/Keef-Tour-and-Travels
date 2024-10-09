9fe7b3dfaba3f68782a570bb03b97274a9d20db7
Banday <Saaifbanday136@gmail.com>
2024-10-09 22:26:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const one =  document.querySelector('.one')
+const two =  document.querySelector('.two')
+const three =  document.querySelector('.three')
+const emptySun = document.querySelector('.emptySun')
+const filledSun = document.querySelector('.sun')
+
+one.addEventListener('mouseover',()=>{
+    two.classList.add('minus')
+    three.classList.add('minus')
+    one.addEventListener('mouseout',()=>{
+        two.classList.remove('minus')
+        three.classList.remove('minus')
+    })
+})
+two.addEventListener('click',()=>{
+    one.classList.add('minus')
+})
+
+two.addEventListener('mouseover',()=>{
+    one.classList.add('minus')
+    three.classList.add('minus')
+    two.addEventListener('mouseout',()=>{
+        one.classList.remove('minus')
+        three.classList.remove('minus')
+    })
+})
+two.addEventListener('click',()=>{
+    one.classList.add('minus')
+})
+
+three.addEventListener('mouseover',()=>{
+    one.classList.add('minus')
+    two.classList.add('minus')
+    three.addEventListener('mouseout',()=>{
+        one.classList.remove('minus')
+        two.classList.remove('minus')
+    })
+})
+
+// dark mode functionality
+emptySun.addEventListener('click',()=>{
+   document.body.classList.toggle('dark')
+   emptySun.classList.toggle('black')
+})
+
+// hidden section functionality
+const seeMoreBtn = document.querySelector('.see-more-btn')
+const hiddenDivs = document.querySelectorAll('.hidden')
+
+seeMoreBtn.addEventListener('click', () => {
+    hiddenDivs.forEach((div) => { 
+            div.style.display = "flex"; 
+    })
+
+    seeMoreBtn.style.display = 'none'; 
+});
+
